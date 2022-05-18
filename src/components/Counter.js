82c92a4365fbedb099cfa3bfe82080b5e2349164
MeghanPund <meghan.pund@gmail.com>
2022-05-18 09:35:24 -0400

    validating prop types with PropTypes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Counter = ({ index, changeScore, score }) => {
-
     return (
       <div className="counter">
         <button className="counter-action decrement" onClick={() => changeScore(index, -1)}> - </button>
@@ -9,6 +9,12 @@ const Counter = ({ index, changeScore, score }) => {
         <button className="counter-action increment" onClick={() => changeScore(index, 1)}> + </button>
       </div>
     );
+}
+
+Counter.propTypes = {
+  index: PropTypes.number,
+  score: PropTypes.number,
+  changeScore: PropTypes.func
 }
 
 export default Counter;
